5c013cbf51745afe5395f0da275a2be03042af1f
Rácz Zalán <zalan.racz.dev@gmail.com>
2025-09-24 00:59:33 +0200

    Adding the error and the not found pages

---
+++ src/app/error.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { SoccerBallIcon } from '@phosphor-icons/react';
+import { JSX } from 'react';
+
+export default function Error({ reset }: { error: Error & { digest?: string }; reset: () => void }): JSX.Element {
+    return (
+        <div className="flex h-full items-center justify-evenly gap-6">
+            <SoccerBallIcon width="250px" height="250px" weight="thin" className="hidden shrink-0 sm:block" />
+            <div className="flex flex-col items-center gap-6">
+                <h2 className="w-full text-4xl">Ez nem feature, hanem bug</h2>
+                <p>
+                    Valami hiba történt a betöltése közben. Próbáld meg újratölteni az oldalt. Akár az is előfordulhat,
+                    hogy segít. De azért ne legyenek nagy reményeid. Ha a hiba továbbra is fennáll, akkor írj egy
+                    emailt:&nbsp;
+                    <a href="mailto:zalan.racz.dev@gmail.com" className="text-sky-800 hover:text-sky-600">
+                        zalan.racz.dev@gmail.com
+                    </a>
+                </p>
+                <button className="rounded-2xl border-1 p-2 hover:bg-black hover:text-white" onClick={() => reset()}>
+                    Oldal újratöltése
+                </button>
+            </div>
+        </div>
+    );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,28 @@
+import { SoccerBallIcon } from '@phosphor-icons/react/dist/ssr';
+import Link from 'next/link';
+import { JSX } from 'react';
+
+export default function NotFound(): JSX.Element {
+    return (
+        <div className="flex h-full items-center justify-evenly gap-6">
+            <SoccerBallIcon width="250px" height="250px" weight="thin" className="hidden shrink-0 sm:block" />
+            <div className="flex flex-col gap-6">
+                <h2 className="text-4xl">Az oldal nem létezik</h2>
+                <div>
+                    <p>
+                        Lehet, hogy elrontottad az URL-t, vagy olyan oldalhoz próbálsz hozzáférni, amihez nincs jogod.
+                        Én pedig nem hagyhatom, hogy hozzáférj a... szupertitkos foci adatokhoz. De a lényeg, hogy a te
+                        hibád. Tuti. Ha mégis ragaszkodsz a tévképzeteidhez, akkor írj egy emailt és szívesen
+                        elmagyarázom, hogy miért a te hibád:&nbsp;
+                        <a href="mailto:zalan.racz.dev@gmail.com" className="text-sky-800 hover:text-sky-600">
+                            zalan.racz.dev@gmail.com
+                        </a>
+                    </p>
+                </div>
+                <Link className="w-full text-center text-sky-800 hover:text-sky-600" href="/">
+                    Visszatérés a főoldalra
+                </Link>
+            </div>
+        </div>
+    );
+}
