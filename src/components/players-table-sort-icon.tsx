aadd6cc51da3dd4919874c6a7491fc23ad01860c
Rácz Zalán <zalan.racz.dev@gmail.com>
2025-10-04 20:58:44 +0200

    Making the players table sort icons more consistent and more accessible

--- src/components/players-table-sort-icon.tsx
+++ src/components/players-table-sort-icon.tsx
@@ -12,10 +12,12 @@ export function PlayersTableSortIcon({
     name: keyof PlayerListDto;
 }): JSX.Element {
     return (
-        <>
+        <div className="text-sky-800 group-hover:text-sky-600" aria-hidden="true">
             {orderBy === name && orderByDirection === 'asc' && <SortAscendingIcon />}
             {orderBy === name && orderByDirection === 'desc' && <SortDescendingIcon />}
-            {orderBy !== name && <SortDescendingIcon className="opacity-0 group-hover:opacity-100" />}
-        </>
+            {orderBy !== name && name === 'name' && <SortAscendingIcon className="hidden group-hover:block" />}
+            {orderBy !== name && name !== 'name' && <SortDescendingIcon className="hidden group-hover:block" />}
+            {orderBy !== name && <div className="h-4 w-4 group-hover:hidden"></div>}
+        </div>
     );
 }
